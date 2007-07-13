e50ffe77713e91196226fce84532ddd2ea503ef1
Anant Narayanan <anant@kix.in>
2007-07-13 23:00:14 +0530

    Default controllers and views

--- registration/app/tmp/cache/persistent/class.paths.php
+++ registration/app/tmp/cache/persistent/class.paths.php
@@ -1,6 +1,8 @@
 <?php
 $config = array();
-$config['Controllers']['Core']['PagesController'] = array('path' => '/Library/WebServer/Documents/cake/cake/libs/controller/pages_controller.php', );
+$config['Controllers']['StudentsController'] = array('path' => '/Library/WebServer/Documents/cake/app/controllers/students_controller.php', );
 $config['Components']['Core']['SessionComponent'] = array('path' => '/Library/WebServer/Documents/cake/cake/libs/controller/components/session.php', );
+$config['Models']['Student'] = array('path' => '/Library/WebServer/Documents/cake/app/models/student.php', );
 $config['Helpers']['Core']['HtmlHelper'] = array('path' => '/Library/WebServer/Documents/cake/cake/libs/view/helpers/html.php', );
+$config['Helpers']['Core']['FormHelper'] = array('path' => '/Library/WebServer/Documents/cake/cake/libs/view/helpers/form.php', );
 $config['Helpers']['Core']['SessionHelper'] = array('path' => '/Library/WebServer/Documents/cake/cake/libs/view/helpers/session.php', );
