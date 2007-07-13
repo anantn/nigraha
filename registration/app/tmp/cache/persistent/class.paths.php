54f2a47d9bd32998d151fb4657277864bfbc88e6
Anant Narayanan <anant@kix.in>
2007-07-13 18:47:58 +0530

    Initial check-in

---
+++ registration/app/tmp/cache/persistent/class.paths.php
@@ -0,0 +1,6 @@
+<?php
+$config = array();
+$config['Controllers']['Core']['PagesController'] = array('path' => '/Library/WebServer/Documents/cake/cake/libs/controller/pages_controller.php', );
+$config['Components']['Core']['SessionComponent'] = array('path' => '/Library/WebServer/Documents/cake/cake/libs/controller/components/session.php', );
+$config['Helpers']['Core']['HtmlHelper'] = array('path' => '/Library/WebServer/Documents/cake/cake/libs/view/helpers/html.php', );
+$config['Helpers']['Core']['SessionHelper'] = array('path' => '/Library/WebServer/Documents/cake/cake/libs/view/helpers/session.php', );
