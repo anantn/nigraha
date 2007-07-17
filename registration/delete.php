be54d7eec5fe816ae70263765e218bd91ef91e44
Anant Narayanan <anant@kix.in>
2007-07-17 10:08:21 +0530

    Quick delete script.

---
+++ registration/delete.php
@@ -0,0 +1,12 @@
+<?php
+
+mysql_connect('localhost', 'cake', 'cakephp');
+mysql_select_db('cake');
+
+$x = mysql_query("DELETE * FROM students WHERE collegeid = '$argv[1]'");
+var_dump($x);
+
+$y = mysql_query("DELETE * FROM courses_students WHERE collegeid = '$argv[1]'");
+var_dump($y);
+
+?>
