00f654d3eea456a81d315d9e399d480ce241729b
Anant Narayanan <anant@kix.in>
2007-07-27 15:11:25 +0530

    Update only courses.

---
+++ registration/scripts/onlycourses.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Checking 'courses_students' table...........\n\n";
+$query = 'SELECT * FROM courses_students';
+$result = mysql_query($query);
+while($entry = mysql_fetch_assoc($result)) {
+	if(strlen($entry['collegeid'])==5) {
+		$query4 = "UPDATE courses_students SET collegeid = '0".$entry['collegeid']."' WHERE (collegeid = '".$entry['collegeid']."' and course_id = '".$entry['course_id']."')";
+		mysql_query($query4) or die(mysql_error());
+		echo $entry['collegeid']." --> 0".$entry['collegeid']."\n";
+	} else if(strlen($entry['collegeid'])==6 AND substr($entry['collegeid'],0,1)!='0') {
+		$query4 = "UPDATE courses_students SET collegeid = '0".$entry['collegeid']."' WHERE (collegeid = '".$entry['collegeid']."' and course_id = '".$entry['course_id']."')";
+		mysql_query($query4) or die(mysql_error());
+		echo $entry['collegeid']." --> 0".$entry['collegeid']."\n";
+	}
+	echo "\n\n";
+}
+
+?>
